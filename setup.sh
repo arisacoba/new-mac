04df34eb3693dff16ef1ecd69864fd48f687ebb6
Aris Acoba <aris.acoba@gmail.com>
2021-12-28 00:09:02 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ brew tap homebrew/cask
 brew install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" # Install oh-my-zsh
 brew install yarn
+brew upgrade vim
 
 # Design
 brew install --cask figma
